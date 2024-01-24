01cd6fe14d95f7be0f42bef54f8ec1642689b25d
Kyungtae Lee <mlogue76@gmail.com>
2024-01-24 20:07:54 +0900

    chapter5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter, Roboto_Mono, Lusitana } from 'next/font/google'
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
+})
